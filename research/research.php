4a9ac768ed66dc4ba94c9c696b6f90ffea2be937
3gomen <35678874+3gomen@users.noreply.github.com>
2018-05-09 12:52:15 +0200

    research init

---
+++ research/research.php
@@ -0,0 +1,196 @@
+@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<!--
+	Verti by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>MKVM</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="homepage">
+		
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row 200%">
+							<div class="4u 12u$(medium)">
+								<div id="sidebar">
+
+									<!-- Sidebar -->
+									
+										<section>
+											<h2>Archív</h2>
+											
+											<h3>Évszám</h3>
+											
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											
+											<h3>Évszám</h3>
+											
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+											<a href="#">Ex-Archív hírek <br></a>
+										</section>
+								</div>
+							</div>
+							<div class="8u 12u$(medium) important(medium)">
+								<div id="content">
+
+									<!-- Content -->
+										<article>
+										
+											<h3>Gyüjteményeink Cikk 1</h3>
+											<p>Magyar Gyűjtők Társasága
+
+												Balta, fejsze, szekerce és bárdgyűjtemény
+
+												A gyűjtemény több mint 500 darabot tartalmaz a fenti kéziszerszámokból. A szerszámok döntő többsége a 19. század második feléből és a 20. század első feléből származik.
+
+												A fő gyűjtési ág mellett  egyéb faipari kéziszerszámok (gyaluk, kézi vonók, vésők kalapácsok stb.) is megtalálhatók a gyűjteményben.
+
+												A gyűjtő tagja a Magyar Gyűjtők Társaságának és a Magyar Famíves Céh Egyesületnek.
+
+												Kapcsolat: recskae@pr.hu
+
+												Szakirodalom:
+
+												Recska Endre:  „Fafeldolgozó vasszerszámok védjegyei”  (Osztrák-Magyar Monarchia és Magyarország, 1876-1945) magánkiadás, 2007.
+
+												Recska Endre: „Kések, borotvák, ollók, vágó- és szúrófegyverek védjegyei” (Osztrák-Magyar Monarchia és Magyarország, 1876-1945) magánkiadás 2011.
+
+												„cme” Varrógép-gyűjtemény
+
+												A 2000 óta létező „cme” gyűjtemény 60-70 db, a. gyűjtő által szakszerűen helyreállított varrógépből és egyéb kiegészítőkből ( olajozók, tartozékdobozok, korabeli dokumentumok, mint pl. számlák, használati utasítások) áll. A gyűjtemény erőssége a professzionális varrógép műszerészi háttér, mely másoknak is jó lehetőség a műszaki problémák elhárítására.
+
+												A gyűjtő tagja a Magyar Gyűjtők Társaságának
+
+												Kapcsolat: Méreg László, maskara45@freemail.hu
+												
+												A „cst” gyűjtemény
+
+												A soproni székhelyű, bő negyven éve szaporodó, s ma már kb. 400 tárgyat számláló faipari szerszámgyűjtemény alapja egy 1968-ban megvásárolt háború előtti asztalos-műhely maradéka. Ez attól fogva folyamatosan bővült asztalos szerszámokkal és ügyeskezű, falusi fúró-faragó emberek egyedi szerszámaival. A cél egy minél teljesebb asztalos szerszámkészlet összeállítása volt, melyhez régi szakkönyvek, mintalapok, árjegyzékek, képek és a szakma múltját érintő könyvrészletek, leírások, adatok, gyűjtése is párosult. A gyűjtemény erőssége a rendszerezettség, a viszonylag bőséges régi szakirodalom – eredeti vagy másolt formában – és egyedi, ún. „sajátkészítésű” szerszámok.
+
+												A gyűjtő tagja a Magyar Gyűjtők Társaságának a Magyar Famíves Céhnek, és a Faipari Tudományos Egyesületnek.
+
+												Kapcsolat: Szemerey Tamás,  tszemerey@fmk.nyme.hu
+
+												Szakirodalom:
+
+												1. Szemerey T.: Az asztalosságban használt idegen szavak gyűjteménye,in: Faipari kézikönyv II. (szerk.: Molnárné Posch Paula) Sopron, 2002. p. 442-446. ISBN 963 00 4231 2; ISBN 963 00 4229 0
+
+												2. Szemerey T: 350 db szakmatörténeti (az asztalosság történetét ismertető) címszó kidolgozása,in: Bútorművészeti lexikon (szerk.: Kiss Éva, Corvina Bp. 2005) ISBN 963 13 5411 3
+
+												3. Szemerey T: 250 db szakszó értelmezése és 450 db, az asztalosságban használt idegen szó értelmezése, ill. magyarázata in: Magyar-német faipari értelmező kéziszótár 29-52. és 158-173. o. (Zala Megyei Vállalkozásfejlesztési Alapítvány Zalaegerszeg, 2005) ISBN 963 219 009 2
+
+												4. Szemerey T: Szerszámábrázolások 1-2.in: Hírfa 2008/2 és 5. (XVIII. évf.) 26-28. és (XVIII. évf.) 30-32. ISSN 1418-432X
+
+												A „czs” gyűjtemény
+
+												A „czs” a nyírség egyik fejlődőképes gyűjteménye, mely már kiállítási háttérrel is rendelkezik. A tulajdonos technikai gyakorlata folytán a gyűjtemény varrógépeket és azok szerkezeti elemei közül hangsúlyosan állványlapokat tartalmaz. Az öntött állványlapok itt nemcsak technikatörténeti, hanem esztétikai jelentőséggel is bírnak, hiszen a korabeli öntvényeket gyakran díszítették gazdag ornamentikák. A dokumentált gyűjtemény mögött jól felszerelt műhely és jelentős lakatosipari, kézműves gyakorlat húzódik meg.
+
+												Gyűjtő: Zomborszki Zsolt,  Magyar Gyűjtők Társasága tagja
+
+												Kapcsolat: sewing1846@gmail.com
+
+												A „cnj” gyűjtemény
+
+												A „cnj” gyűjteményben elsősorban asztalosok által használt szerszámok találhatók. A gyaluk, vésők, fűrészek, fúrók, enyvező edények, mérőeszközök mellett a fafeldolgozó mesterségek (kádár, ács, bognár, faesztergályos) szerszámai teszik teljessé a kollekciót.
+
+												A gyűjtő a szerszámokhoz kapcsolódva, mesterség bemutatókat is szervez, ahol az őskortól napjainkig használt, de elfelejtett famegmunkálási munkafolyamatokat rekonstruálják.
+
+												A gyűjtő tagja a Magyar Gyűjtők Társaságának.
+
+												Kapcsolat: Németh János, czudar.nemeth@freemail.hu
+
+												A „ctg” gyűjtemény 250 db történelmi, elsősorban 19.századi varrógépet tartalmaz, mely 68 gyártót ölel fel a világ varrógépgyártásából, 1864-től 1950-ig. A gyűjtemény erényei közé sorolható, hogy több nemzetközi vonatkozásban is ritka darabot mondhat magának és szakszerű feldolgozottsága lehetőséget biztosít akár tudományos kutatásra is.
+
+												A gyűjtő tagja a Magyar Gyűjtők Társaságának, a német Schlingenfaenger és az angol ISMACS társaságoknak.
+
+												Kapcsolat: Tóth György,  bautex@enternet.hu
+
+												Magyar nyelvű szakirodalom: Tóth György: Gyűszűtől a varrógépig, magánkiadás 2008, Tóth György: Varrógépek gyűjtése, magánkiadás 2009, Tóth György: A varrógép enciklopédiája, magánkiadás 2011</p>
+
+											<h3>Adatbázisok</h3>
+											<ul class="style2">
+												<li><a href="#">Amet turpis, feugiat et sit amet</a></li>
+												<li><a href="#">Ornare in hendrerit in lectus</a></li>
+												<li><a href="#">Semper mod quis eget mi dolore</a></li>
+												<li><a href="#">Quam turpis feugiat sit dolor</a></li>
+											</ul>
+											
+											<h3>Terms of Use</h3>
+											<p>This is <strong>Verti</strong>, a free and fully responsive HTML5 site template by <a href="http://html5up.net">HTML5 UP</a>.
+											Verti is released under the <a href="http://html5up.net/license">Creative Commons Attribution license</a>
+											, so feel free to use it for any personal or commercial project you might have going on (just don't forget to credit us for the design!)</p>
+											<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper bibendum ipsum
+											, et tristique augue fringilla eu. Vivamus id risus vel dolor auctor euismod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat.
+											Aliquam luctus mattis lectus sit amet phasellus quam turpis.</p>
+
+										</article>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
